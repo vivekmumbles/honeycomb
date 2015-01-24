b55ba10925c99c796398fcc0befa092dc662af19
Vivek Ramadoss <keviv93@gmail.com>
2015-01-24 00:41:59 -0700

    centered

--- touch.js
+++ touch.js
@@ -1,6 +1,8 @@
 
-window.onload = function() {
+var center = function() {
     var sqr = document.getElementById("square");
     sqr.style.marginTop = window.innerHeight/2 - 100 + "px";
     sqr.style.marginLeft = window.innerWidth/2 - 100 + "px";
 }
+
+window.onload = window.onresize = center;
