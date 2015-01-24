169108180b5bbdf212a5c0a92d3dea2731f80205
Vivek Ramadoss <keviv93@gmail.com>
2015-01-24 00:40:02 -0700

    centered

---
+++ touch.js
@@ -0,0 +1,6 @@
+
+window.onload = function() {
+    var sqr = document.getElementById("square");
+    sqr.style.marginTop = window.innerHeight/2 - 100 + "px";
+    sqr.style.marginLeft = window.innerWidth/2 - 100 + "px";
+}
